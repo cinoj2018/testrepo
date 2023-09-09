5edde2516f9b704f4621db6cbf07940f6a1b5e91
cinoj2018 <126408155+cinoj2018@users.noreply.github.com>
2023-09-09 10:03:05 +0530

    Update main.tf with aws variables

--- main.tf
+++ main.tf
@@ -2,7 +2,9 @@
 # SPDX-License-Identifier: MPL-2.0
 
 provider "aws" {
-  region = var.region
+access_key = "${var.AWS_ACCESS_KEY_ID}"
+secret_key = "${var.AWS_SECRET_ACCESS_KEY}"
+region     = "${var.AWS_REGION}"
 }
 
 data "aws_ami" "ubuntu" {
